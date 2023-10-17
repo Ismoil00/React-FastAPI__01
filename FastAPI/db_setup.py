6b96a196c3e92a9ba8bdaf31f00ba7241356cba5
Ismoil <juraev.abubakr_98@mail.ru>
2023-10-17 16:59:31 +0500

    initiation

---
+++ FastAPI/db_setup.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DB_URL = "postgresql://postgres:ismoil0101@localhost:5432/FastAPI_React_01"
+
+engine = create_engine(DB_URL, connect_args={"check_same_threads": False})
+
+SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+Base = declarative_base()
